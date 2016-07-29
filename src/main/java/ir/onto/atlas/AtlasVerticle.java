feea76af53c88ef0337ff441873109f8d53be613
Morteza Ansarinia <ansarinia@me.com>
2016-07-29 15:20:14 +0430

    Initial Maven project with Vert.x, owlapi, and test dependencies.

---
+++ src/main/java/ir/onto/atlas/AtlasVerticle.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ *        File: OntologyVerticle.java
+ *    Revision: 1
+ * Description: Ontology verticle.
+ *      Author: Morteza Ansarinia <ansarinia@me.com>
+ *  Created on: Jul 29, 2016
+ *     Project: onto.cogatlas
+ *   Copyright: See the file "LICENSE" for the full license governing this code.
+ *******************************************************************************/
+package ir.onto.atlas;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
+public class AtlasVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Future<Void> fut) {
+
+    Router router = Router.router(vertx);
+    
+    router.route().handler(rc -> {
+      // This handler gets called for each request that arrives on the server
+      HttpServerResponse response = rc.response();
+      response.putHeader("content-type", "text/plain");
+
+      // Write to the response and end it
+      response.end("Hello from Vert.x Web!");      
+    });
+
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+    
+    /*vertx
+        .createHttpServer()
+        .requestHandler(r -> {
+          r.response().end("<h1>Hello from first CogAtlas.</h1>");
+        })
+        .listen(8080, result -> {
+          if (result.succeeded()) {
+            fut.complete();
+          } else {
+            fut.fail(result.cause());
+          }
+        });
+        */
+  }
+}
---
+++ src/main/java/ir/onto/atlas/OntologyVerticle.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ *        File: OntologyVerticle.java
+ *    Revision: 1
+ * Description: Ontology verticle.
+ *      Author: Morteza Ansarinia <ansarinia@me.com>
+ *  Created on: Jul 29, 2016
+ *     Project: onto.cogatlas
+ *   Copyright: See the file "LICENSE" for the full license governing this code.
+ *******************************************************************************/
+package ir.onto.atlas;
+
+import io.vertx.core.AbstractVerticle;
+
+public class OntologyVerticle extends AbstractVerticle {
+  @Override
+  public void start() {
+    
+  }
+}
