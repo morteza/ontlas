4924356ac01c57383ff3f785a97fe9d83314f323
Morteza Ansarinia <ansarinia@me.com>
2016-08-19 12:26:22 +0430

    Minor ontology improvements and static file handler.

--- src/main/java/ir/onto/ontlas/Starter.java
+++ src/main/java/ir/onto/ontlas/Starter.java
@@ -5,6 +5,8 @@ package ir.onto.ontlas;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 import ir.onto.ontlas.verticles.EchoServiceVerticle;
 import ir.onto.ontlas.verticles.WebApplicationVerticle;
 import ir.onto.ontlas.verticles.semantic.OntologyInfoVerticle;
@@ -17,10 +19,14 @@ public class Starter extends AbstractVerticle {
     configureLogging();
     
     Vertx vertx = Vertx.vertx();
+    Router router = Router.router(vertx);
     
     // Services go here!
     vertx.deployVerticle(new WebApplicationVerticle());
     vertx.deployVerticle(new EchoServiceVerticle());
     vertx.deployVerticle(new OntologyInfoVerticle());
+    
+    // Serve static resources from the /assets directory
+    router.route("/assets/*").handler(StaticHandler.create("assets"));
   }
 }
